679397bfc80c0e4921a096d93d339cbf008ea753
nabepa <nabepadev@gmail.com>
2021-05-11 23:46:38 +0900

    add change mode

--- src/app.jsx
+++ src/app.jsx
@@ -2,17 +2,37 @@ import styles from './app.module.css';
 import React, { useEffect, useState } from 'react';
 import Timmer from './components/timmer/timmer';
 
+const FOCUS_TIME = 2;
+const RELAX_TIME = 1;
+
 function App() {
   const [isPlay, setIsPlay] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [mode, setMode] = useState('focus');
+  const [timeLeft, setTimeLeft] = useState(FOCUS_TIME * 2);
 
-  const onClick = () => {
+  const handdleStart = () => {
     setIsPlay(!isPlay);
   };
 
   const handdleStop = () => {
     setIsPlay(false);
-    setTimeLeft(25 * 60);
+    setMode('focus');
+    setTimeLeft(FOCUS_TIME * 2);
+  };
+
+  const changeMode = () => {
+    switch (mode) {
+      case 'focus':
+        setMode('relax');
+        setTimeLeft(RELAX_TIME * 2);
+        break;
+      case 'relax':
+        setMode('focus');
+        setTimeLeft(FOCUS_TIME * 2);
+        break;
+      default:
+        throw new Error(`${mode} is undefinded mode`);
+    }
   };
 
   useEffect(() => {
@@ -20,13 +40,18 @@ function App() {
       const timer = setTimeout(() => {
         setTimeLeft(timeLeft - 1);
       }, 1000);
+
+      if (timeLeft == -1) {
+        changeMode();
+      }
+
       return () => clearTimeout(timer);
     }
   });
 
   return (
     <>
-      <button onClick={onClick}>Start</button>
+      <button onClick={handdleStart}>Start</button>
       <button onClick={handdleStop}>Reset</button>
       <div>
         {Math.floor(timeLeft / 60)} MIN {timeLeft % 60} SEC
