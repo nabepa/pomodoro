22f90dc8d54b0801ed06506397a1e97e5bc336a3
nabepa <nabepadev@gmail.com>
2021-05-11 23:07:21 +0900

    Add start/pause button for timmer

--- src/app.jsx
+++ src/app.jsx
@@ -3,18 +3,25 @@ import React, { useEffect, useState } from 'react';
 import Timmer from './components/timmer/timmer';
 
 function App() {
+  const [isPlay, setIsPlay] = useState(false);
   const [timeLeft, setTimeLeft] = useState(25 * 60);
 
+  const onClick = () => {
+    setIsPlay(!isPlay);
+  };
+
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(timeLeft - 1);
-    }, 1000);
-    return () => clearTimeout(timer);
+    if (isPlay) {
+      const timer = setTimeout(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
   });
 
   return (
     <>
-      <button>Start</button>
+      <button onClick={onClick}>Start</button>
       <div>
         {Math.floor(timeLeft / 60)} MIN {timeLeft % 60} SEC
       </div>
